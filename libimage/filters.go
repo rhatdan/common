d02c59576226e1ef15c1c571b01db57193d39835
Daniel J Walsh <dwalsh@redhat.com>
2021-12-10 16:02:21 -0500

    Support reference as disjoint filters
    Reference filters should be handled as disjoint, while
    the others are required. This will allow callers to pass
    multip reference filters and if an image matches it is returned.
    
    Other filters the image has to match all filters to be returned.
    
    [NO NEW TESTS NEEDED] I will open up a validation against buildah to
    make sure this passes before merging.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- libimage/filters.go
+++ libimage/filters.go
@@ -21,14 +21,25 @@ type filterFunc func(*Image) (bool, error)
 
 // filterImages returns a slice of images which are passing all specified
 // filters.
-func filterImages(images []*Image, filters []filterFunc) ([]*Image, error) {
-	if len(filters) == 0 {
+func (r *Runtime) filterImages(ctx context.Context, images []*Image, options *ListImagesOptions) ([]*Image, error) {
+	if len(options.Filters) == 0 || len(images) == 0 {
 		return images, nil
+	}
+
+	disjoints, filters, err := r.compileImageFilters(ctx, options)
+	if err != nil {
+		return nil, err
 	}
 	result := []*Image{}
 	for i := range images {
-		include := true
+		include := false
 		var err error
+		for _, filter := range disjoints {
+			include, err = filter(images[i])
+			if err != nil {
+				return nil, err
+			}
+		}
 		for _, filter := range filters {
 			include, err = filter(images[i])
 			if err != nil {
@@ -48,7 +59,7 @@ func filterImages(images []*Image, filters []filterFunc) ([]*Image, error) {
 // compileImageFilters creates `filterFunc`s for the specified filters.  The
 // required format is `key=value` with the following supported keys:
 //           after, since, before, containers, dangling, id, label, readonly, reference, intermediate
-func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOptions) ([]filterFunc, error) {
+func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOptions) ([]filterFunc, []filterFunc, error) {
 	logrus.Tracef("Parsing image filters %s", options.Filters)
 
 	var tree *layerTree
@@ -64,11 +75,12 @@ func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOp
 	}
 
 	filterFuncs := []filterFunc{}
+	disjointFuncs := []filterFunc{}
 	for _, filter := range options.Filters {
 		var key, value string
 		split := strings.SplitN(filter, "=", 2)
 		if len(split) != 2 {
-			return nil, errors.Errorf("invalid image filter %q: must be in the format %q", filter, "filter=value")
+			return nil, nil, errors.Errorf("invalid image filter %q: must be in the format %q", filter, "filter=value")
 		}
 
 		key = split[0]
@@ -78,14 +90,14 @@ func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOp
 		case "after", "since":
 			img, _, err := r.LookupImage(value, nil)
 			if err != nil {
-				return nil, errors.Wrapf(err, "could not find local image for filter %q", filter)
+				return nil, nil, errors.Wrapf(err, "could not find local image for filter %q", filter)
 			}
 			filterFuncs = append(filterFuncs, filterAfter(img.Created()))
 
 		case "before":
 			img, _, err := r.LookupImage(value, nil)
 			if err != nil {
-				return nil, errors.Wrapf(err, "could not find local image for filter %q", filter)
+				return nil, nil, errors.Wrapf(err, "could not find local image for filter %q", filter)
 			}
 			filterFuncs = append(filterFuncs, filterBefore(img.Created()))
 
@@ -94,21 +106,21 @@ func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOp
 			case "false", "true":
 			case "external":
 				if options.IsExternalContainerFunc == nil {
-					return nil, fmt.Errorf("libimage error: external containers filter without callback")
+					return nil, nil, fmt.Errorf("libimage error: external containers filter without callback")
 				}
 			default:
-				return nil, fmt.Errorf("unsupported value %q for containers filter", value)
+				return nil, nil, fmt.Errorf("unsupported value %q for containers filter", value)
 			}
 			filterFuncs = append(filterFuncs, filterContainers(value, options.IsExternalContainerFunc))
 
 		case "dangling":
 			dangling, err := strconv.ParseBool(value)
 			if err != nil {
-				return nil, errors.Wrapf(err, "non-boolean value %q for dangling filter", value)
+				return nil, nil, errors.Wrapf(err, "non-boolean value %q for dangling filter", value)
 			}
 			t, err := getTree()
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			filterFuncs = append(filterFuncs, filterDangling(ctx, dangling, t))
 
@@ -118,11 +130,11 @@ func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOp
 		case "intermediate":
 			intermediate, err := strconv.ParseBool(value)
 			if err != nil {
-				return nil, errors.Wrapf(err, "non-boolean value %q for intermediate filter", value)
+				return nil, nil, errors.Wrapf(err, "non-boolean value %q for intermediate filter", value)
 			}
 			t, err := getTree()
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			filterFuncs = append(filterFuncs, filterIntermediate(ctx, intermediate, t))
 
@@ -132,31 +144,31 @@ func (r *Runtime) compileImageFilters(ctx context.Context, options *ListImagesOp
 		case "readonly":
 			readOnly, err := strconv.ParseBool(value)
 			if err != nil {
-				return nil, errors.Wrapf(err, "non-boolean value %q for readonly filter", value)
+				return nil, nil, errors.Wrapf(err, "non-boolean value %q for readonly filter", value)
 			}
 			filterFuncs = append(filterFuncs, filterReadOnly(readOnly))
 
 		case "reference":
-			filterFuncs = append(filterFuncs, filterReferences(value))
+			disjointFuncs = append(disjointFuncs, filterReferences(value))
 
 		case "until":
 			ts, err := timetype.GetTimestamp(value, time.Now())
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			seconds, nanoseconds, err := timetype.ParseTimestamps(ts, 0)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			until := time.Unix(seconds, nanoseconds)
 			filterFuncs = append(filterFuncs, filterBefore(until))
 
 		default:
-			return nil, errors.Errorf("unsupported image filter %q", key)
+			return nil, nil, errors.Errorf("unsupported image filter %q", key)
 		}
 	}
 
-	return filterFuncs, nil
+	return disjointFuncs, filterFuncs, nil
 }
 
 // filterReferences creates a reference filter for matching the specified value.
